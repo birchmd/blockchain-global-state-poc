eaae13eb41594cc4b8ea8a0924a4c6993b82e5f3
Michael Birch <birchmd@casperlabs.io>
2018-11-23 20:59:54 +0000

    implement external write method using pointers to wasm memory

--- src/engine.rs
+++ src/engine.rs
@@ -1,19 +1,26 @@
 extern crate wasmi;
 
+use self::wasmi::memory_units::Pages;
 use self::wasmi::{
     Error as InterpreterError, Externals, FuncInstance, FuncRef, HostError, ImportsBuilder,
-    ModuleImportResolver, ModuleInstance, ModuleRef, RuntimeArgs, RuntimeValue, Signature, Trap,
-    ValueType,
+    MemoryInstance, MemoryRef, ModuleImportResolver, ModuleInstance, ModuleRef, RuntimeArgs,
+    RuntimeValue, Signature, Trap, ValueType,
 };
 use super::storage::key::Key;
 use super::storage::value::Value;
 use super::storage::{Error as StorageError, ExecutionEffect, GlobalState, TrackingCopy};
+
+use std::collections::HashSet;
 use std::fmt;
 
 #[derive(Debug)]
 pub enum Error {
     Interpreter(InterpreterError),
     Storage(StorageError),
+    InvalidKeyIndicator(u32),
+    InvalidValueIndicator(u32),
+    ValueTypeSizeMismatch { value_type: u32, value_size: usize },
+    ForgedReference(Key),
 }
 
 impl fmt::Display for Error {
@@ -37,13 +44,85 @@ impl From<StorageError> for Error {
 impl HostError for Error {}
 
 pub struct Runtime<'a, T: TrackingCopy + 'a> {
-    account: [u8; 20],
+    memory: MemoryRef,
+    known_urefs: HashSet<Key>,
     state: &'a mut T,
 }
 
 impl<'a, T: TrackingCopy + 'a> Runtime<'a, T> {
     fn effect(&self) -> ExecutionEffect {
         self.state.effect()
+    }
+
+    fn key_from_mem(&mut self, key_type: u32, key_ptr: u32) -> Result<Key, Error> {
+        match key_type {
+            0 => {
+                let mut buf = [0u8; 20];
+                self.memory.get_into(key_ptr, &mut buf[..])?;
+                Ok(Key::Account(buf))
+            }
+            1 => {
+                let mut buf = [0u8; 32];
+                self.memory.get_into(key_ptr, &mut buf[..])?;
+                Ok(Key::Hash(buf))
+            }
+            2 => {
+                let mut buf = [0u8; 32];
+                self.memory.get_into(key_ptr, &mut buf[..])?;
+                let key = Key::URef(buf);
+                if self.known_urefs.contains(&key) {
+                    Ok(key)
+                } else {
+                    Err(Error::ForgedReference(key))
+                }
+            }
+            _ => Err(Error::InvalidKeyIndicator(key_type)),
+        }
+    }
+
+    fn value_from_mem(
+        &mut self,
+        value_type: u32,
+        value_ptr: u32,
+        value_size: usize,
+    ) -> Result<Value, Error> {
+        match value_type {
+            0 => {
+                if value_size == 4 {
+                    let mut buf = [0u8; 4];
+                    self.memory.get_into(value_ptr, &mut buf[..])?;
+                    let i: i32 = unsafe { std::mem::transmute(buf) }; //TODO: can this be better?
+                    Ok(Value::Int32(i))
+                } else {
+                    Err(Error::ValueTypeSizeMismatch {
+                        value_type,
+                        value_size,
+                    })
+                }
+            }
+            1 => {
+                let bytes = self.memory.get(value_ptr, value_size)?;
+                Ok(Value::ByteArray(bytes))
+            }
+            //TODO: add other value subtypes
+            _ => Err(Error::InvalidValueIndicator(value_type)),
+        }
+    }
+
+    pub fn write(&mut self, args: RuntimeArgs) -> Result<(), Trap> {
+        //args(0) = key type (must be one of 0, 1, 2)
+        //args(1) = pointer to key in wasm memory
+        //args(2) = value type (must be one of ...)
+        //args(3) = pointer to (serialized) value
+        //args(4) = value size (in bytes)
+        let key_type: u32 = args.nth_checked(0)?;
+        let key_ptr: u32 = args.nth_checked(1)?;
+        let key = self.key_from_mem(key_type, key_ptr)?;
+        let value_type: u32 = args.nth_checked(2)?;
+        let value_ptr: u32 = args.nth_checked(3)?;
+        let value_size: u32 = args.nth_checked(4)?;
+        let value = self.value_from_mem(value_type, value_ptr, value_size as usize)?;
+        self.state.write(key, value).map_err(|e| e.into())
     }
 }
 
@@ -58,9 +137,7 @@ impl<'a, T: TrackingCopy + 'a> Externals for Runtime<'a, T> {
     ) -> Result<Option<RuntimeValue>, Trap> {
         match index {
             WRITE_FUNC_INDEX => {
-                let idx: i32 = args.nth(0);
-                self.state
-                    .write(Key::Account(self.account), Value::Int32(idx))?;
+                let _ = self.write(args)?;
                 Ok(None)
             }
             _ => panic!("unknown function index"),
@@ -78,7 +155,7 @@ impl<'a> ModuleImportResolver for RuntimeModuleImportResolver {
     ) -> Result<FuncRef, InterpreterError> {
         let func_ref = match field_name {
             "write" => FuncInstance::alloc_host(
-                Signature::new(&[ValueType::I32][..], None),
+                Signature::new(&[ValueType::I32; 5][..], None),
                 WRITE_FUNC_INDEX,
             ),
             _ => {
@@ -103,13 +180,17 @@ pub fn instantiate(module: wasmi::Module) -> Result<ModuleRef, Error> {
 
 pub fn exec<T: TrackingCopy, G: GlobalState<T>>(
     instance: ModuleRef,
-    account: [u8; 20],
+    account_addr: [u8; 20],
     gs: &G,
 ) -> Result<ExecutionEffect, Error> {
+    //Note: each "page" is 64 KiB
+    let memory = MemoryInstance::alloc(Pages(4), Some(Pages(4))).unwrap();
+    let account = gs.get(&Key::Account(account_addr))?.as_account();
     let mut state = gs.tracking_copy();
     let mut runtime = Runtime {
-        account,
+        memory,
         state: &mut state,
+        known_urefs: account.urefs(),
     };
     let _ = instance.invoke_export("call", &[], &mut runtime)?;
 
--- src/storage/value.rs
+++ src/storage/value.rs
@@ -1,12 +1,21 @@
+use std::collections::{HashMap, HashSet};
 use std::fmt;
 use std::string::String;
 
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub enum Value {
     Int32(i32),
+    ByteArray(Vec<u8>),
     ListInt32(Vec<i32>),
     String(String),
-    ByteArray(Vec<u8>),
+    Acct(Account),
+}
+
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct Account {
+    public_key: [u8; 32],
+    nonce: u64,
+    known_urefs: HashMap<String, super::Key>,
 }
 
 use self::Value::*;
@@ -18,7 +27,25 @@ impl Value {
             ListInt32(_) => "List[Int32]".to_string(),
             String(_) => "String".to_string(),
             ByteArray(_) => "ByteArray".to_string(),
+            Acct(_) => "Account".to_string(),
         }
+    }
+
+    pub fn as_account(&self) -> &Account {
+        match self {
+            Acct(a) => a,
+            _ => panic!("Not an account: {}", self),
+        }
+    }
+}
+
+impl Account {
+    pub fn urefs(&self) -> HashSet<super::Key> {
+        let mut result = HashSet::new();
+        for r in self.known_urefs.values() {
+            result.insert(r.clone());
+        }
+        result
     }
 }
 
