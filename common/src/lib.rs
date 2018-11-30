bd6b831e81a9b8417f4b2b7ca17c26102eecadad
Michael Birch <birchmd@casperlabs.io>
2018-11-30 17:52:03 +0000

    make example contract a little more complex; change wasm ffi to use raw byte pointers; add high-level safe functions over unsafe ffi

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -12,14 +12,43 @@ pub mod key;
 pub mod memio;
 pub mod value;
 
+mod ext_ffi {
+    extern "C" {
+        pub fn read(key_ptr: *const u8, value_ptr: *mut u8);
+        pub fn write(key_ptr: *const u8, value_ptr: *const u8);
+        pub fn new_uref(key_ptr: *mut u8);
+    }
+}
+
 pub mod ext {
+    use super::alloc::alloc::{Alloc, Global};
+    use super::ext_ffi;
     use crate::key::Key;
     use crate::value::Value;
 
-    extern "C" {
-        pub fn read(key: &Key) -> Value;
-        pub fn write(key: &Key, value: &Value);
-        pub fn new_uref() -> Key;
+    pub fn read(key: &Key) -> Value {
+        let key_ptr = key as *const Key;
+        let value_ptr: *mut Value = Global.alloc_one().unwrap().as_ptr();
+        unsafe {
+            ext_ffi::read(key_ptr as *const u8, value_ptr as *mut u8);
+            core::ptr::read(value_ptr)
+        }
+    }
+
+    pub fn write(key: &Key, value: &Value) {
+        let key_ptr = key as *const Key;
+        let value_ptr = value as *const Value;
+        unsafe {
+            ext_ffi::write(key_ptr as *const u8, value_ptr as *const u8);
+        }
+    }
+
+    pub fn new_uref() -> Key {
+        let key_ptr: *mut Key = Global.alloc_one().unwrap().as_ptr();
+        unsafe {
+            ext_ffi::new_uref(key_ptr as *mut u8);
+            core::ptr::read(key_ptr)
+        }
     }
 }
 
--- contracts/src/lib.rs
+++ contracts/src/lib.rs
@@ -6,10 +6,16 @@ use common::value::Value;
 
 #[no_mangle]
 pub fn call() {
-    let x = Value::Int32(0);
-    let key = unsafe{ new_uref() };
-    unsafe { write(&key, &x) };
-    let y = unsafe { read(&key) };
-    unsafe { write(&new_uref(), &y) };
-    assert_eq!(x, y);
+    let x = Value::Int32(1234);
+    let y = Value::Int32(8910);
+    let x_key = new_uref();
+    let y_key = new_uref();
+    write(&x_key, &x);
+    write(&y_key, &y);
+    let xr = read(&x_key);
+    let yr = read(&y_key);
+    write(&new_uref(), &xr);
+    write(&new_uref(), &yr);
+    assert_eq!(x, xr);
+    assert_eq!(y, yr);
 }
