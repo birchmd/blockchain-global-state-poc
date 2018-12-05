58b706a3ebc41a3408ee8e74e9a3ddedf55c80c6
Michael Birch <birchmd@casperlabs.io>
2018-12-05 18:37:37 +0000

    Write simple serializer/deserializer for Key and Value types; use serializer/deserializer to move objects between host and wasm memories

--- contracts/src/lib.rs
+++ contracts/src/lib.rs
@@ -1,16 +1,19 @@
 #![no_std]
+#![feature(alloc)]
+
+#[macro_use]
+extern crate alloc;
 
 extern crate common;
 use common::ext::*;
 use common::value::Value;
 
 #[no_mangle]
-pub fn call() {
+pub extern fn call() {
     let x = 1234;
     let y = 8910;
     let x_key = new_uref();
-    write(&x_key, &Value::Int32(x));
-    add(&x_key, &Value::Int32(y));
+    write(&x_key, &Value::ListInt32(vec![x, y]));
     let xy = read(&x_key);
-    assert_eq!(xy, Value::Int32(x + y));
+    assert_eq!(xy, Value::ListInt32(vec![x, y]));
 }
