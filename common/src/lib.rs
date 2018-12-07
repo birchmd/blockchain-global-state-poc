3462b09b0facc2e2e91a977ce5c5e42b3f6db90d
birchmd <birch@pyrofex.net>
2018-12-07 16:09:23 -0500

    set up ffi for calling on chain contracts; set up example contract to call a function as if it were external

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -14,6 +14,10 @@ pub mod value;
 
 mod ext_ffi {
     extern "C" {
+        //TODO: replace size_of* idiom with one in which the calls are explicitly
+        //      paired. E.g. read_value(key_ptr: *const u8, key_size: usize) -> usize;
+        //                   get_read(value_ptr: *mut u8, value_size: usize);
+        //      Note that the second call does not make sense except after the first one.
         pub fn size_of_value(key_ptr: *const u8, key_size: usize) -> usize;
         pub fn read(key_ptr: *const u8, key_size: usize, value_ptr: *mut u8, value_size: usize);
         pub fn write(key_ptr: *const u8, key_size: usize, value_ptr: *const u8, value_size: usize);
@@ -25,6 +29,23 @@ mod ext_ffi {
             name_size: usize,
             dest_ptr: *mut u8,
             dest_size: usize,
+        );
+        pub fn size_of_arg(i: u32) -> usize;
+        pub fn get_arg(i: u32, dest: *mut u8, size: usize);
+        pub fn ret(value_ptr: *const u8, value_size: usize) -> !;
+        pub fn size_of_call_result(
+            fn_ptr: *const u8,
+            fn_size: usize,
+            args_ptr: *const u8,
+            args_size: usize,
+        ) -> usize;
+        pub fn call_contract(
+            fn_ptr: *const u8,
+            fn_size: usize,
+            args_ptr: *const u8,
+            args_size: usize,
+            res_ptr: *mut u8,
+            res_size: usize,
         );
     }
 }
@@ -38,7 +59,7 @@ pub mod ext {
     use super::ext_ffi;
     use blake2::digest::{Input, VariableOutput};
     use blake2::VarBlake2b;
-    use crate::bytesrepr::BytesRepr;
+    use crate::bytesrepr::{deserialize, BytesRepr};
     use crate::key::{Key, UREF_SIZE};
     use crate::value::Value;
 
@@ -63,7 +84,7 @@ pub mod ext {
             ext_ffi::read(key_ptr, key_size, value_ptr, value_size);
             core::slice::from_raw_parts(value_ptr, value_size)
         };
-        Value::from_bytes(value_bytes).unwrap().0
+        deserialize(value_bytes).unwrap()
     }
 
     pub fn write(key: &Key, value: &Value) {
@@ -88,17 +109,26 @@ pub mod ext {
             ext_ffi::new_uref(key_ptr);
             core::slice::from_raw_parts(key_ptr, UREF_SIZE)
         };
-        Key::from_bytes(slice).unwrap().0
+        deserialize(slice).unwrap()
     }
 
-    pub fn store_function(name: &String) -> Key {
+    fn fn_bytes_by_name(name: &String) -> Vec<u8> {
         let (name_ptr, name_size, _bytes) = to_ptr(name);
         let fn_size = unsafe { ext_ffi::function_size(name_ptr, name_size) };
         let fn_ptr = alloc_bytes(fn_size);
-        let fn_bytes = unsafe {
+        unsafe {
             ext_ffi::function_bytes(name_ptr, name_size, fn_ptr, fn_size);
             Vec::from_raw_parts(fn_ptr, fn_size, fn_size)
-        };
+        }
+    }
+
+    pub fn fn_by_name(name: &String) -> Value {
+        let fn_bytes = fn_bytes_by_name(name);
+        Value::Contract(fn_bytes)
+    }
+
+    pub fn store_function(name: &String) -> Key {
+        let fn_bytes = fn_bytes_by_name(name);
         let mut hasher = VarBlake2b::new(32).unwrap();
         hasher.input(&fn_bytes);
         let mut fn_hash = [0u8; 32];
@@ -107,6 +137,40 @@ pub mod ext {
         let value = Value::Contract(fn_bytes);
         write(&key, &value);
         key
+    }
+
+    pub fn get_arg<T: BytesRepr>(i: u32) -> T {
+        let arg_size = unsafe { ext_ffi::size_of_arg(i) };
+        let dest_ptr = alloc_bytes(arg_size);
+        let arg_bytes = unsafe {
+            ext_ffi::get_arg(i, dest_ptr, arg_size);
+            core::slice::from_raw_parts(dest_ptr, arg_size)
+        };
+        deserialize(arg_bytes).unwrap()
+    }
+
+    pub fn ret<T: BytesRepr>(t: &T) -> ! {
+        let (ptr, size, _bytes) = to_ptr(t);
+        unsafe {
+            ext_ffi::ret(ptr, size);
+        }
+    }
+
+    pub fn call_contract<T: BytesRepr>(contract: &Value, args: &Vec<Vec<u8>>) -> T {
+        if let Value::Contract(c) = contract {
+            let (fn_ptr, fn_size, _bytes) = to_ptr(c);
+            let (args_ptr, args_size, _bytes2) = to_ptr(args);
+            let res_size =
+                unsafe { ext_ffi::size_of_call_result(fn_ptr, fn_size, args_ptr, args_size) };
+            let res_ptr = alloc_bytes(res_size);
+            let res_bytes = unsafe {
+                ext_ffi::call_contract(fn_ptr, fn_size, args_ptr, args_size, res_ptr, res_size);
+                core::slice::from_raw_parts(res_ptr, res_size)
+            };
+            deserialize(res_bytes).unwrap()
+        } else {
+            panic!("{:?} is not a contract!", contract);
+        }
     }
 }
 
--- contracts/src/lib.rs
+++ contracts/src/lib.rs
@@ -6,12 +6,19 @@ extern crate alloc;
 use alloc::string::String;
 
 extern crate common;
+use common::bytesrepr::BytesRepr;
 use common::ext::*;
 use common::value::Value;
 
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
+
 #[no_mangle]
-pub extern "C" fn plus_one(x: i32) -> i32 {
-	x + 1
+pub extern "C" fn plus_one_ext() {
+    let x: i32 = get_arg(0);
+    let y = plus_one(x);
+    ret(&y);
 }
 
 #[no_mangle]
@@ -30,6 +37,9 @@ pub extern "C" fn call() {
     let s_read = read(&x_key);
     assert_eq!(s, s_read);
 
-	let export_name = String::from("plus_one");
-	let _ = store_function(&export_name);
+    let export_name = String::from("plus_one_ext");
+    let contract = fn_by_name(&export_name);
+    let args = vec![3i32.to_bytes()];
+    let result: i32 = call_contract(&contract, &args);
+    assert_eq!(result, 4);
 }
